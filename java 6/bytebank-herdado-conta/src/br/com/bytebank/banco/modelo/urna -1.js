f0b126ed5aed834ed28470d70d0f01d0a0569a14
Wagner dos santos <47212801+wbatista985@users.noreply.github.com>
2022-12-29 22:44:51 -0300

    initial commit

---
+++ java 6/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/urna -1.js
@@ -0,0 +1,43 @@
+
+
+
+let voto = {
+    regiao : "NORDESTE",
+    candidato : "LULA LADRÃO",
+    quantidade : 1000
+} ;
+
+if (voto.regiao == "NORDESTE" && voto.candidato == "LULA LADRÃO") {
+
+        total = voto.quantidade * 2
+} else {
+
+        total = voto.quantidade * 0.5
+}
+
+
+console.log (total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
